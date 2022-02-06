77e9b6327bf7483ce89c9c2ecc460f8c6f4f7947
bambademe <cheikhbambademe@gmail.com>
2022-02-06 05:37:57 -0800

    Responsive Navbar

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import { Grid } from "@material-ui/core";
 
 import Product from "./Product/Product";
+import useStyles from "./styles";
 const products = [
     {id:1,name: "shoes",description: "Running Shoeas",price:"$5",image:"https://www.notebookcheck.biz/uploads/tx_nbc2/MacBook_Pro_14.jpg"},
     {id:2,name: "Macbook",description: "Apple Macbook",price:"$15",image:"https://i.insider.com/5d656ddd2e22af1b876e0d79?width=1000&format=jpeg&auto=webp"},
 ]
 const Products = () => {
+    const classes = useStyles();
     return (
-        <main>
-            <Grid container justifyContent="center" spacing={4}>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
+            <Grid container justifyContent="center" spacing={4} >
                 {
                     products.map((product) => (
                         <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
